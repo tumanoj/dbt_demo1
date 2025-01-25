051a278b6b0b1cc3dd881ea4294465cc32d9c4ad
Manoj Uthaman <tumanoj@gmail.com>
2025-01-25 15:37:49 +0000

    aggregation column modified

--- macros/aggregate_sales.sql
+++ macros/aggregate_sales.sql
@@ -1,11 +1,11 @@
 -- macros/aggregate_sales.sql
 {% macro aggregate_sales(table) %}
   select
-    ORDERID as ORDER_ID,
+    ORDER_ID,
     PAYMENT_ID,
     PAYMENTMETHOD,
     sum(AMOUNT) as TOTAL_SALES
   from {{ table }} 
   where STATUS='success'
-  group by ORDERID,PAYMENT_ID, PAYMENTMETHOD
+  group by ORDER_ID,PAYMENT_ID, PAYMENTMETHOD
 {% endmacro %}
